c50b44dc76f8812f98a55112d0010d1f43981c32
l1ngq <balykhin.maxim@gmail.com>
2026-02-17 00:22:47 +0300

    mistake in path

--- lab-1/src/main/java/com/l1ngq/labs/Main.java
+++ lab-1/src/main/java/com/l1ngq/labs/Main.java
@@ -1,4 +1,4 @@
-package com.karandashov.labs;
+package com.l1ngq.labs;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
