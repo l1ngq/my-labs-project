545899946dd812b4c09c52b082ed45b37a0ed07d
l1ngq <balykhin.maxim@gmail.com>
2026-03-31 01:54:39 +0300

    new lab

---
+++ lab3/src/main/java/com/l1ngq/labs/repository/StudentMapper.java
@@ -0,0 +1,17 @@
+package com.l1ngq.labs.repository;
+
+import com.l1ngq.labs.entity.Student;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentMapper implements RowMapper<Student> {
+    @Override
+    public Student map(ResultSet rs, StatementContext ctx) throws SQLException {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        double grade = rs.getDouble("grade");
+        return new Student(id, name, grade);
+    }
+}
